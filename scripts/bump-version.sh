e20e932850d33d1d1f791a9683ab6733a63fe582
Chad Whitacre <chadwhitacre@sentry.io>
2023-11-17 11:11:29 -0500

    Relicense under FSL-1.0-Apache-2.0

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,6 +4,5 @@ set -eu
 NEW_VERSION="$1"
 
 sed -i -e "s/^\# Self-Hosted Codecov .*/# Self-Hosted Codecov $NEW_VERSION/" README.md
-sed -i -e "s/\(Change Date:\s*\)[-0-9]\+\$/\\1$(date +'%Y-%m-%d')/" LICENSE
 
 echo "New version: $NEW_VERSION"
